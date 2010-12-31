ff11a42f9e9d691ffff540a197ab9cfaab8797f9
Brian Shaver <brian@4-dogs.biz>
2010-12-31 07:33:10 -0500

    Initial Revision

---
+++ dynamic_ip.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#==============================================================================
+# Simple script to monitor for IP address changes
+#==============================================================================
+LAST_IP_FILE=~/.dynip_last
+ADMIN=shakerlxxv@gmail.com
+
+# read last IP
+LAST_ADDR=`cat ${LAST_IP_FILE} 2>/dev/null`
+CUR_ADDR=`/sbin/ip addr show eth0 | grep -w inet | /usr/bin/awk '{print $2}'`
+
+date >> ~/.dynip_log
+
+if [ "$LAST_ADDR" != "$CUR_ADDR" ]; then
+  if [ "$1" = "set" ]; then
+    echo $CUR_ADDR > $LAST_IP_FILE
+  else
+    printf "New IP: $CUR_ADDR\n" | mailx -s "DYNAMIC IP CHANGE" $ADMIN
+  fi
+fi
