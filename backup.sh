a61d5cbb4e56851fcbd251931ac02efcebc999b2
Brian Shaver <brian@4-dogs.biz>
2010-12-31 22:49:17 -0500

    combined rsync options

--- backup.sh
+++ backup.sh
@@ -5,7 +5,7 @@
 #rsync -avz --modify-window=1 --exclude='Music/[J-Z]*' /mnt/Music /media/ION\ DRIVE\ C
 #rsync -avz --modify-window=1 --include='Music/[J-Z]*' --exclude='Music/*' /mnt/Music /media/ION\ DRIVE\ B
 
-RS_OPT="-avz --modify-window=1"
+RS_OPT="/usr/bin/rsync -avz --modify-window=1"
 ADMIN=shakerlxxv@gmail.com
 
 function check_mount () {
@@ -16,20 +16,20 @@ function check_mount () {
 
 { check_mount /mnt/Documents &&
   check_mount /media/BIGDISK4 &&
-  rsync -avz --modify-window=1 /mnt/Documents /media/BIGDISK4; } ||
+  ${RS_OPT} /mnt/Documents /media/BIGDISK4; } ||
 echo "Documents backup failure" | mailx -s "BACKUP FAILURE" ${ADMIN}
 
 { check_mount /mnt/Photos &&
   check_mount /media/ION\ DRIVE\ A/ &&
-  rsync -avz --modify-window=1 --exclude 'Video' /mnt/Photos /media/ION\ DRIVE\ A; } ||
+  ${RS_OPT} --exclude 'Video' /mnt/Photos /media/ION\ DRIVE\ A; } ||
 echo "Photos backup failure" | mailx -s "BACKUP_FAILURE" ${ADMIN}
 
 { check_mount /mnt/Music &&
   check_mount /media/ION\ DRIVE\ C/ &&
-  rsync -avz --modify-window=1 --exclude='Music/[J-Z]*' /mnt/Music /media/ION\ DRIVE\ C; } ||
+  ${RS_OPT} --exclude='Music/[J-Z]*' /mnt/Music /media/ION\ DRIVE\ C; } ||
 echo "Music #1 backup failure" | mailx -s "BACKUP FAILURE" ${ADMIN}
 
 { check_mount /mnt/Music &&
   check_mount /media/ION\ DRIVE\ B &&
-  rsync -avz --modify-window=1 --include='Music/[J-Z]*' --exclude='Music/*' /mnt/Music /media/ION\ DRIVE\ B; } ||
+  ${RS_OPT} --include='Music/[J-Z]*' --exclude='Music/*' /mnt/Music /media/ION\ DRIVE\ B; } ||
 echo "Music #2 backup failure" | mailx -s "BACKUP FAILURE" ${ADMIN}
