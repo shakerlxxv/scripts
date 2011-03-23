8504a55e2e72e6b2807da24c01234349c0fe324e
Brian Shaver <brian@4-dogs.biz>
2011-03-22 23:18:41 -0400

    changed home backup strategy

--- backup-home.sh
+++ backup-home.sh
@@ -3,9 +3,9 @@
 # for specific applications.
 #    1. backup evolution to an external disk.
 ADMIN=shakerlxxv@gmail.com
-BACKUP_FILE=/mnt/Documents/4DogsTechnologies/BriansNotes/evolution-backup.tar.gz
-# evolution requires a display
-export DISPLAY=:0.0
+BACKUP_PARTITION=/media/BRIANSHAVER
+BACKUP_BASE=${BACKUP_PARTITION}/Backup
+RS_OPT="/usr/bin/rsync -avz --modify-window=1"
 
 function check_mount () {
     _MNT_SRC=$1
@@ -14,27 +14,8 @@ function check_mount () {
 }
 
 #
-# 1. backup evolution
+# backup bshaver home
 #
-{ check_mount /mnt/Documents &&
-   #/usr/libexec/evolution/2.32/evolution-backup --backup ${BACKUP_FILE}; } ||
-   evolution --quit &&
-   rm -f ${HOME}/.local/share/evolution/.running &&
-   gconftool-2 --dump /apps/evolution > ${HOME}/.local/share/evolution/backup-restore-gconf.xml &&
-   cd ${HOME} && tar chf - .local/share/evolution .config/evolution .camel_certs evolution.dir | gzip > "${BACKUP_FILE}" &&
-   rm -f ${HOME}/evolution.dir; } ||
-  echo "Evolution backup failure" | mailx -s "BACKUP_FAILURE Evolution" ${ADMIN}
-
-#
-# 2. backup Hamster ( time tracking )
-#
-{ check_mount /mnt/Documents &&
-  cp /home/bshaver/.local/share/hamster-applet/hamster.db /mnt/Documents/4DogsTechnologies/BriansNotes/; } ||
-    echo "Time Tracking Backup Failure" | mailx -s "BACKUP FAILURE Time Tracking" ${ADMIN}
-
-#
-# 3. backup gnote ( journal and ideas )
-#
-{ check_mount /mnt/Documents &&
-    cd /home/bshaver/.local/share && tar czf /mnt/Documents/4DogsTechnologies/BriansNotes/gnote-backup.tar.gz gnote; } ||
-      echo "Gnote Backup Failure" | mailx -s "BACKUP FAILURE Gnote" ${ADMIN}
+{ check_mount ${BACKUP_PARTITION} &&
+  ${RS_OPT} --exclude 'Downloads' --exclude '.cache' /home/bshaver ${BACKUP_BASE}; } ||
+echo "Home backup failure" | mailx -s "BACKUP_FAILURE" ${ADMIN}
