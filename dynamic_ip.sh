26f5c103a58614e410855b0a8b97a0c1b39d15aa
Brian Shaver <brian@4-dogs.biz>
2011-08-21 09:03:18 -0400

    added logging of current ip

--- dynamic_ip.sh
+++ dynamic_ip.sh
@@ -10,8 +10,6 @@ LAST_ADDR=`cat ${LAST_IP_FILE} 2>/dev/null`
 #CUR_ADDR=`/sbin/ip addr show eth0 | grep -w inet | /usr/bin/awk '{print $2}'`
 CUR_ADDR=`/usr/bin/wget -qO - http://192.168.1.1/htmlV/index.asp | grep "IPPassThruState_IPAddress =" | awk -F\" '{print $2}'`
 
-date >> ~/.dynip_log
-
 if [ "$LAST_ADDR" != "$CUR_ADDR" ]; then
   if [ "$1" = "set" ]; then
     echo $CUR_ADDR > $LAST_IP_FILE
@@ -19,3 +17,6 @@ if [ "$LAST_ADDR" != "$CUR_ADDR" ]; then
     printf "New IP: $CUR_ADDR\n" | mailx -s "DYNAMIC IP CHANGE" $ADMIN
   fi
 fi
+
+echo "`date` ${CUR_ADDR}" >> ~/.dynip_log
+
